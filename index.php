3ef011cc2632e91e47f4a31f9cc449d5d329e8ba
alexw <alexw@DESKTOP-D7VSHBH>
2020-06-24 21:38:31 -0300

    primeiro commit

--- index.php
+++ index.php
@@ -2,12 +2,58 @@
 <!-- -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+	<meta charset="utf-8" />
+	<title>Projeto Base</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/estilo.css" />
+        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/bootstrap.js"></script>	
+	<link rel="icon" href="iconeX1.ico" type="imagens/x-icon" />
+	<!--Bootstrap-->
+	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
     </head>
     <body>
-        <?php
-       echo "casa";
-        ?>
+        <!-- cabeçalho -->    
+        <header>
+            <div class="cabecalho container">
+                <div class="row">
+                    CABEÇALHO
+                </div>
+            </div>
+        </header>
+        <!-- fim do cabeçalho -->   
+        
+        <!-- ------------------------------ --> 
+        
+        <!-- corpo -->
+        <section class="conteudo container">
+            <div class="row">
+                CONTEÚDO
+            </div>
+        </section>
+        <!-- fim do corpo -->
+        
+        <!-- ------------------------------ -->  
+        
+        <!-- rodape -->   
+        <footer class="rodape container">
+            <div class="row">
+                RODAPE
+            </div>
+        </footer>
+        <!-- fim do rodape -->        
+        
+        <!-- ------------------------------ -->    
+        
     </body>
 </html>
